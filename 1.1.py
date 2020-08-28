d8711ffabdc8e83431d127e68f6312eafe26a88b
kskolosova <68472065+kskolosova@users.noreply.github.com>
2020-08-28 09:19:09 +0300

    1 задание
    Выполнено 1 задание к 1 уроку

---
+++ 1.1.py
@@ -0,0 +1,17 @@
+text = 'Hello world!'
+print(text)
+
+number = 1044
+print(number)
+
+number_fl = 13.44
+print(number_fl)
+
+my_bool = 4 < 9
+print(my_bool)
+
+
+name = input('Введите имя: ')
+surname = input('Введите фамилию: ')
+age = input('Введите ваш возраст: ')
+print('Ваши данные: ', surname, name, 'возраст: ', age)
